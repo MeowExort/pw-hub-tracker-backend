37b49263703ca20dde6587433d6e034b6c9544c6
MeowExort <tobilts@yandex.ru>
2026-03-20 20:50:17 +0300

    feat(api): добавить спецификацию OpenAPI и настроить CORS для локальной разработки

--- src/Pw.Hub.Tracker.Api/Program.cs
+++ src/Pw.Hub.Tracker.Api/Program.cs
@@ -3,6 +3,12 @@ using Pw.Hub.Tracker.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins("http://localhost:5174")
+            .AllowAnyHeader()
+            .AllowAnyMethod()));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.MapControllers();
 app.Run();
